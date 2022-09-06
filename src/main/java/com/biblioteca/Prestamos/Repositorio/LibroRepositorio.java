3d921f9167b1826fdae73d8b0aeefe38c9ad03e8
LuisPec0 <luisperezcotes@gmail.com>
2022-09-06 18:11:03 -0500

    actualización back

--- src/main/java/com/biblioteca/Prestamos/Repositorio/LibroRepositorio.java
+++ src/main/java/com/biblioteca/Prestamos/Repositorio/LibroRepositorio.java
@@ -6,7 +6,11 @@ import com.biblioteca.Prestamos.Entidades.Libro;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.ArrayList;
+
 @Repository
 public interface LibroRepositorio extends JpaRepository<Libro,String> {
+
+    ArrayList<Libro> findByAutor(String autor);
 
 }
--- src/main/java/com/biblioteca/Prestamos/Servicio/LibroServicio.java
+++ src/main/java/com/biblioteca/Prestamos/Servicio/LibroServicio.java
@@ -28,5 +28,45 @@ public class LibroServicio {
 
     }
 
+    public ArrayList<Libro> BuscarAutor(String autor){
+        return repositorio.findByAutor(autor);
+    }
 
+    public  String agregarLibro(Libro libro){
+        if(!(buscarLibro(libro.getISBN()).isPresent())) {
+            repositorio.save(libro);
+            return "El libro se agregó exitosamente";
+        }else {
+            return "El libro se encuentra registarado en la base de Datos.";
+        }
+    }
+
+    public String actualizarLibro(Libro libro){
+        if(buscarLibro(libro.getISBN()).isPresent()){
+            repositorio.save(libro);
+            return "Libro actualizado correctamente";
+        }else{
+            return "El libro a modificar no existe";
+        }
+    }
+
+    public String actualizarEditorial(String ISBN, String editorial){
+        if(buscarLibro(ISBN).isPresent()){
+            Libro libro = repositorio.findById(ISBN).get();
+            libro.setEditorial(editorial);
+            repositorio.save(libro);
+            return "La editoria fue cambiada con exito";
+        }else{
+            return "El libro no se pudo actualizar";
+        }
+    }
+
+    public String eliminarLibro(String isbn){
+        if(buscarLibro(isbn).isPresent()){
+            repositorio.deleteById(isbn);
+            return "El libro se eliminó con exito";
+        }else {
+            return "El libro a eliminar no existe";
+        }
+    }
 }
