d1b303f5653b292e496458c2f5c600abafa15202
LuisPec0 <luisperezcotes@gmail.com>
2022-09-06 17:59:45 -0500

    actualizacon de backend

--- src/main/java/com/biblioteca/Prestamos/Controlador/LibroControlador.java
+++ src/main/java/com/biblioteca/Prestamos/Controlador/LibroControlador.java
@@ -2,9 +2,7 @@ package com.biblioteca.Prestamos.Controlador;
 
 import com.biblioteca.Prestamos.Entidades.Libro;
 import com.biblioteca.Prestamos.Servicio.LibroServicio;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
 import java.util.Optional;
@@ -26,5 +24,30 @@ public class LibroControlador {
     @GetMapping("/BuscarLibro/{id}")
     public Optional<Libro> buscarLibro(@PathVariable("id")String ISBN){
         return servicio.buscarLibro(ISBN);
+    }
+
+    @GetMapping("/BuscarAutor/{autor}")
+    public ArrayList<Libro> BuscarAutor(@PathVariable("autor")String autor){
+    return servicio.BuscarAutor(autor);
+    }
+
+    @PostMapping("/agregarlibro")
+    public String agregarlibro(@RequestBody Libro libro){
+        return servicio.agregarLibro(libro);
+    }
+
+    @PutMapping("/actualizarlibro")
+    public  String actualizarlibro(@RequestBody Libro libro){
+        return servicio.actualizarLibro(libro);
+    }
+
+    @PatchMapping("/actualizarEdit/{isbn}/{editorial}")
+    public String actualizarEdit(@PathVariable("isbn")String ISBN, @PathVariable("editorial") String editorial){
+        return servicio.actualizarEditorial(ISBN, editorial);
+    }
+
+    @DeleteMapping("/eliminarlibro/{id}")
+    public String eliminarLibro(@PathVariable("id") String ISBN){
+        return servicio.eliminarLibro(ISBN);
     }
 }
