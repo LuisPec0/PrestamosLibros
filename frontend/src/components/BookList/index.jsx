967f46f910c571790033eba22e6f45730b97264a
Diego Ramirez Ramirez <diegoandres9927@gmail.com>
2022-09-11 17:45:18 -0500

    Cambios en frontend

--- frontend/src/components/BookList/index.jsx
+++ frontend/src/components/BookList/index.jsx
@@ -1,6 +1,8 @@
 import React, { useEffect, useState } from 'react'
 import { listarlibros } from '../../services/db'
+import 'bootstrap/dist/css/bootstrap.min.css'
 import './style.css'
+import { Button, Card, Modal, Spinner } from 'react-bootstrap'
 
 export const BookList = () => {
 
@@ -11,26 +13,52 @@ export const BookList = () => {
     console.log(books)
   },[])
 
+
   const getBooks = async() => {
     const arrBooks = await listarlibros()
     setBooks(arrBooks)
   }
 
-  
-
+ 
 
   return (
-    <div className='container__cards'>
-     {books !== undefined ?
-     (books.map(({titulo, editorial, autor, numeroPaginas, isbn})=>(
-      <div key={isbn} className='cards'>
-        <h1 className='cards__title'>{titulo}</h1>
-        <h3 className='cards__content' >Editorial: {editorial}</h3>
-        <h4 className='cards__content' >Paginas: {numeroPaginas}</h4>
-        <h4 className='cards__content' >Autor: {autor}</h4>
+    <div className="container">
+      <div className="col-5 d-flex">
+        {books !== undefined ? (
+          books.map(({ titulo, editorial, autor, numeroPaginas, isbn }) => (
+            <Card
+              key={isbn}
+              text="dark"
+              style={{ width: "20rem", marginRight: "30px" }}
+              className="cards mb-2"
+            >
+              <Card.Header>
+                <Card.Title className="text-center">{titulo}</Card.Title>
+              </Card.Header>
+              <Card.Body>
+                <Card.Text>
+                  <strong>Paginas:</strong> {numeroPaginas}
+                  <br />
+                  <strong>Autor:</strong> {autor}
+                  <br />
+                  <strong>Editorial:</strong> {editorial}
+                  <br />
+                  <strong>Isbn:</strong> {isbn}
+                </Card.Text>
+              </Card.Body>
+              <Card.Footer className="text-center">
+                <Button className="buttons">Info</Button>
+              </Card.Footer>
+            </Card>
+          ))
+        ) 
+        :(
+          <div className='justify-center text center'>
+            <Spinner animation="border" variant="secondary" />
+          </div>
+          )
+        }
       </div>
-    )))
-    :('no hay datos')}
     </div>
-  )
+  );
 }
--- frontend/src/components/Menu/Menudesktop/index.jsx
+++ frontend/src/components/Menu/Menudesktop/index.jsx
@@ -2,6 +2,8 @@ import './style.css'
 import Books  from '../../../assets/Books.png'
 import AddButton  from '../../../assets/AddButton.png'
 import HomeButton  from '../../../assets/Home.png'
+import AddUser  from '../../../assets/AddUser.png'
+import User  from '../../../assets/User.png'
 import { Link } from 'react-router-dom'
 
 
@@ -9,9 +11,11 @@ export const Menudesktop = () => {
   return (
     <div className='nav'>
       <div className='nav__container__links'>
-        <Link to='/' className='nav__container__icons'><img className='nav__icons' src={HomeButton}/></Link>
         <Link to='/booklist' className='nav__container__icons'><img className='nav__icons' src={Books}/></Link>
         <Link to='/addbook' className='nav__container__icons'><img className='nav__icons' src={AddButton}/></Link>     
+        <Link to='/' className='nav__container__icons'><img className='nav__icons' src={HomeButton}/></Link>
+        <Link to='/users' className='nav__container__icons'><img className='nav__icons' src={User}/></Link>
+        <Link to='/addUser' className='nav__container__icons'><img className='nav__icons' src={AddUser}/></Link>
       </div>
     </div>
   )
